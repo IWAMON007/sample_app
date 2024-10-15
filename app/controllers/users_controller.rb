7593b4fbc56e237448fe0ed1ff35624ef47c26a7
IWAMON007 <123219601+IWAMON007@users.noreply.github.com>
2024-10-15 09:52:10 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
   def create
-    @user = User.new(user_params)    # 実装は終わっていないことに注意!
+    @user = User.new(user_params)  
     if @user.save
-      # 保存の成功をここで扱う。
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
     else
       render 'new', status: :unprocessable_entity
     end
