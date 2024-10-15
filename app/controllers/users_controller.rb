ccd0740c3cd24b3eccdbd96bee74c714c8a285a8
IWAMON007 <123219601+IWAMON007@users.noreply.github.com>
2024-10-15 08:43:03 +0000

    7.2章まで終了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
   def new
   end
+  def show
+    @user = User.find(params[:id])
+  end
+  def create
+    @user = User.new(params[:user])    # 実装は終わっていないことに注意!
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
 end
