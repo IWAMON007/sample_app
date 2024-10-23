265429d9435cae0b82ed6d63476ea3ae3d65c637
IWAMON007 <123219601+IWAMON007@users.noreply.github.com>
2024-10-23 09:39:26 +0000

    8.2.7

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,6 +23,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     end
     follow_redirect!
     assert_template 'users/show'
-    assert flash[:success] == "Welcome to the Sample App!"
+    assert is_logged_in?
   end
 end
